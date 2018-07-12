d4b8aa80426e528b1235cfe19524c9fe06e27e46
陈双 <chenshuang@banggood.com>
2018-07-12 09:53:44 +0800

    The crash about dispatch_group.
    The crash about dispatch_group.

--- Dispatch_group_crash/ViewController.m
+++ Dispatch_group_crash/ViewController.m
@@ -9,7 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+/** 数组 */
+@property(nonatomic, strong)dispatch_group_t group;
 @end
 
 @implementation ViewController
@@ -19,11 +20,55 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    if (self.group == nil) {
+        dispatch_group_t group = dispatch_group_create();
+        self.group = group;
+    }
+    
+    dispatch_group_enter(self.group);
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        sleep(2);
+        dispatch_group_leave(self.group);
+    });
+    
+    dispatch_group_enter(self.group);
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        sleep(2);
+        dispatch_group_leave(self.group);
+    });
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    dispatch_group_enter(self.group);
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        sleep(2);
+        dispatch_group_leave(self.group);
+    });
+
+    dispatch_group_notify(self.group, dispatch_get_main_queue(), ^{
+        NSLog(@"操作全部完成");
+    });
 }
 
+//- (void)errorRequest {
+//    dispatch_group_t group = dispatch_group_create();
+//    self.group = group;
+//
+//    // enter code
+//    [request  requestGetUrl:url success:^(id responds) {
+//        // leave code
+//    }];
+//}
+//
+//- (void)rightRequest {
+//    if (self.group == nil) {
+//        dispatch_group_t group = dispatch_group_create();
+//        self.group = group;
+//    }
+//
+//    // enter code
+//    [request  requestGetUrl:url success:^(id responds) {
+//        // leave code
+//    }];
+//}
 
 @end
